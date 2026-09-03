deaa197c37dd99dc6d2de2f90557aecad3a8ea6d
farishilhams <farishilham.s@gmail.com>
2026-09-03 12:54:24 +0700

    chore: add dev proxy in vite.config.ts and dev:all script in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,18 @@ export default defineConfig({
     svgr({
       svgrOptions: {
         icon: true,
-        // This will transform your SVG to a React component
         exportType: "named",
         namedExport: "ReactComponent",
       },
     }),
   ],
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+    },
+  },
 });
